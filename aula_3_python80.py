09362d370b88597b3ecb6cd3ae6e94e69c9279fa
Rafael Oliveira <147670578+oliveirarafaa@users.noreply.github.com>
2024-03-09 17:47:50 -0300

    Add files via upload

---
+++ aula_3_python80.py
@@ -0,0 +1,34 @@
+#variavel do tipo real -> float
+real = 1.5
+print(real)
+
+#variavel do tipo texto
+texto = 'Isso é um texto'
+
+#variavel do tipo boleano #1 V #0 F
+booleano1 = True
+booleano2 = False
+
+#variavel do tipo inteiro
+numero_inteiro = 10
+
+
+
+#VARIAVÉL COM LETRA MAIÚSCULA
+NUM = 10
+print(NUM)
+
+#VARIAVÉL COM LETRA MINUSCULA
+num = 20
+print(num)
+
+#VARIAVÉL DECLARADA COM NÚMERO
+num2 = 15
+print(num2)
+
+#VARIAVÉL COM UNDERLINE
+_num3 = 30
+print(_num3)
+
+num_3 = 35
+print(num_3)
---
+++ aula_3_python80_2.py
@@ -0,0 +1,7 @@
+#contatenação -> juntar as informações de output
+nome = 'Fernando'
+idade = 18
+endereco = 'Rua 70'
+print('Nome:',nome)
+print('Idade:',idade)
+print('Endereço:',endereco)
